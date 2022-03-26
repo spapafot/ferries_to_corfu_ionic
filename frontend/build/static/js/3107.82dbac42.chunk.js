33626c552bb3800367b00a0dbf9eeb8562c0490e
spapafot <spapafot@gmail.com>
2022-03-26 21:41:15 +0200

    basic

---
+++ frontend/build/static/js/3107.82dbac42.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 3107.82dbac42.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkftc=self.webpackChunkftc||[]).push([[3107],{3107:function(c,n,t){t.r(n),t.d(n,{g:function(){return u}});var u=t(7923).i}}]);
+//# sourceMappingURL=3107.82dbac42.chunk.js.map
---
+++ frontend/build/static/js/3127.4213d3c1.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 3127.4213d3c1.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkftc=self.webpackChunkftc||[]).push([[3127],{3127:function(t,e,n){n.r(e),n.d(e,{i:function(){return r}});var r=function(t){return t&&""!==t.dir?"rtl"===t.dir.toLowerCase():"rtl"===(null===document||void 0===document?void 0:document.dir.toLowerCase())}}}]);
+//# sourceMappingURL=3127.4213d3c1.chunk.js.map
